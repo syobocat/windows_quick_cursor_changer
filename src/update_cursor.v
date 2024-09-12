3ba55d301bbece845009ee9c2dd5f6e17d1cb334
SyoBoN <syobon@syobon.net>
2024-09-12 16:36:04 +0900

    better error

--- src/update_cursor.v
+++ src/update_cursor.v
@@ -6,11 +6,12 @@ const spi_setcursors = 0x0057
 const spif_updateinifile = 0x0001
 const spif_sendchange = 0x0002
 
-fn C.SystemParametersInfoW(uiAction u32, uiParam u32, pvParam voidptr, fWinIni u32) int
+fn C.SystemParametersInfoW(uiAction u32, uiParam u32, pvParam voidptr, fWinIni u32) bool
+fn C.GetLastError() u32
 
 fn update_cursor() ! {
-	code := C.SystemParametersInfoW(spi_setcursors, 0, unsafe { nil }, spif_updateinifile | spif_sendchange)
-	if code == 0 {
-		return error('カーソルの更新に失敗しました')
+	if !C.SystemParametersInfoW(spi_setcursors, 0, unsafe { nil }, spif_updateinifile | spif_sendchange) {
+		status := C.GetLastError()
+		return error('カーソルの更新に失敗しました: コード${status}')
 	}
 }
