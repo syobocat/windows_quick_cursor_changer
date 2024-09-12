5a1854c488b7338e59865f7d414840f98f595d50
SyoBoN <syobon@syobon.net>
2024-09-12 15:39:41 +0900

    null

--- src/update_cursor.v
+++ src/update_cursor.v
@@ -9,7 +9,7 @@ const spif_sendchange = 0x02
 fn C.SystemParametersInfoW(uiAction u32, uiParam u32, pvParam voidptr, fWinIni u32) bool
 
 fn update_cursor() ! {
-	if !C.SystemParametersInfoW(spi_setcursors, 0, 0, spif_updateinifile | spif_sendchange) {
+	if !C.SystemParametersInfoW(spi_setcursors, 0, unsafe { nil }, spif_updateinifile | spif_sendchange) {
 		return error('カーソルの更新に失敗しました')
 	}
 }
